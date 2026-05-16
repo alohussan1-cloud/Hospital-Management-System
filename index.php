166b826409cb02a1f2084be7fc16b1076d4232ab
Ali Hassan <alohussan1@gmail.com>
2026-05-16 22:03:04 +0500

    Basic front end

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
   <title>HMS - Hospital Management System</title>
   <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400;600;700&family=DM+Sans:wght@300;400;500;600&display=swap" rel="stylesheet"/>
-<link rel="stylesheet" href="teststyle.css">
+<link rel="stylesheet" href="style.css">
 </head>
 <body>
 
